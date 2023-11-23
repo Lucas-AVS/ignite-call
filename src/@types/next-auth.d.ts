3d6d6b153192d6635a21b9d3c3a037e7b29630a2
Ponk8 <lucas.alencar31@gmail.com>
2023-11-23 01:54:49 -0300

    feat: Persist time intervals

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth'
-
 declare module 'next-auth' {
   export interface User {
     id: string
@@ -7,5 +6,9 @@ declare module 'next-auth' {
     email: string
     username: string
     avatar_url: string
+  }
+
+  interface Session {
+    user: User
   }
 }
